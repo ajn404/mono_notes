c35f53322fd6da3759b8c6b69afb514871686f77
ajn404 <huiyuening00@gmail.com>
2023-08-23 14:59:20 +0800

    add hutao

--- docs/src/content/config.ts
+++ docs/src/content/config.ts
@@ -5,16 +5,6 @@ const philosophy = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    // Transform string to Date object
-    pubDate: z
-      .string()
-      .or(z.date())
-      .transform((val) => new Date(val)),
-    updatedDate: z
-      .string()
-      .optional()
-      .transform((str) => (str ? new Date(str) : undefined)),
-    heroImage: z.string().optional(),
   }),
 });
 
