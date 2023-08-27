044b846681ec5472d811a1003401ef82acec92c8
ajn404 <huiyuening00@gmail.com>
2023-08-27 18:45:39 +0800

    docs not end

--- docs/src/components/menu/Example.tsx
+++ docs/src/components/menu/Example.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { useRef } from "react";
-import { motion, sync, useCycle } from "framer-motion";
+import { useRef, useEffect, useState } from "react";
+import { motion, sync, useCycle, useAnimate, stagger } from "framer-motion";
 import { useDimensions } from "./use-dimensions.ts";
 import { MenuToggle } from "./MenuToggle";
 import { Navigation } from "./Navigation";
 import "./style.scss";
-
+import { Menu } from "./Menu";
 const sidebar = {
   open: (height = 1000) => ({
     clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
@@ -26,22 +26,60 @@ const sidebar = {
   },
 };
 
+function useMenuAnimation(isOpen: boolean) {
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    const menuAnimations = isOpen
+      ? [
+          [
+            "nav",
+            { transform: "translateX(0%)" },
+            { ease: [0.08, 0.65, 0.53, 0.96], duration: 0.6 },
+          ],
+          [
+            "li",
+            { transform: "scale(1)", opacity: 1, filter: "blur(0px)" },
+            { delay: stagger(0.05), at: "-0.1" },
+          ],
+        ]
+      : [
+          [
+            "li",
+            { transform: "scale(0.5)", opacity: 0, filter: "blur(10px)" },
+            { delay: stagger(0.05, { from: "last" }), at: "<" },
+          ],
+          ["nav", { transform: "translateX(-100%)" }, { at: "-0.1" }],
+        ];
+
+    animate([
+      [
+        "path.top",
+        { d: isOpen ? "M 3 16.5 L 17 2.5" : "M 2 2.5 L 20 2.5" },
+        { at: "<" },
+      ],
+      ["path.middle", { opacity: isOpen ? 0 : 1 }, { at: "<" }],
+      [
+        "path.bottom",
+        { d: isOpen ? "M 3 2.5 L 17 16.346" : "M 2 16.346 L 20 16.346" },
+        { at: "<" },
+      ],
+      ...menuAnimations,
+    ]);
+  }, [isOpen]);
+
+  return scope;
+}
+
 export const Example = () => {
-  const [isOpen, toggleOpen] = useCycle(false, true);
-  const containerRef = useRef(null);
-  const { height } = useDimensions(containerRef);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const scope = useMenuAnimation(isOpen);
 
   return (
-    <motion.nav
-      initial={false}
-      animate={isOpen ? "open" : "closed"}
-      custom={height}
-      ref={containerRef}
-      className="example"
-    >
-      <motion.div className="background" variants={sidebar} />
-      <Navigation />
-      <MenuToggle toggle={() => toggleOpen()} />
-    </motion.nav>
+    <div ref={scope}>
+      <Menu />
+      <MenuToggle toggle={() => setIsOpen(!isOpen)} />
+    </div>
   );
 };
