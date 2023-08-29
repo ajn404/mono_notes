7f2fdb76aed59aa4b7c202571d83f65ac0748106
ajn404 <huiyuening00@gmail.com>
2023-08-29 18:23:04 +0800

    feat: add download

--- docs/src/components/doodle/common.tsx
+++ docs/src/components/doodle/common.tsx
@@ -1,24 +1,38 @@
 import 'css-doodle';
-import { useEffect, useState,type  ReactNode,type ReactElement} from 'react';
+import {
+    useEffect, useState, type ReactNode, type ReactElement, useRef
+} from 'react';
 
 interface Props {
-    children: ReactNode
+    children: ReactNode,
+    download?: Boolean
 }
 
-const Doodle: React.FunctionComponent<Props> = ({ children }) => {
-     let value = (children as ReactElement).props.value;
-    let [show,setShow] = useState(false)
-     const click = () => {
+const Doodle: React.FunctionComponent<Props> = ({ children, download }) => {
+    let value = (children as ReactElement).props.value;
+    let doodle = useRef<any>(null)
+    let [show, setShow] = useState(false)
+    const click = () => {
         setShow(show => !show);
     }
     useEffect(() => {
         setShow(true)
     })
-  return (
-      show&& <css-doodle onClick={click}>
-      {value}
-    </css-doodle>
-  );
+    const downloadClick = () => {
+        doodle.current &&
+            doodle.current.export({
+                scale: 8,
+                download: true
+            });
+    }
+    return (
+        <div>
+            {show && <css-doodle ref={doodle} onClick={click}>
+                {value}
+            </css-doodle>}
+            {download && <button className='w-full pt-5 decoration-sky-500 text-xl hover:drop-shadow-md' onClick={downloadClick}>下载</button>}
+        </div>
+    );
 };
 
 export default Doodle;
