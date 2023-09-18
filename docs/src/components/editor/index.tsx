1f32f52ef8ada538da8edb01e2b3424e9ecb333f
ajn404 <huiyuening00@gmail.com>
2023-09-18 17:39:21 +0800

    feat: try to add a fantastic editor

---
+++ docs/src/components/editor/index.tsx
@@ -0,0 +1,15 @@
+import '@blocksuite/editor/themes/affine.css';
+import { useEffect, useRef } from 'react';
+
+export default () => {
+    const ref = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        import('@blocksuite/editor').then((res) => {
+            const editor = new res.SimpleAffineEditor();
+            ref.current?.appendChild(editor)
+        })
+    })
+    return <>
+        <div className="editor" ref={ref}></div>
+    </>
+}
