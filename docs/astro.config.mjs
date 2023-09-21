8c150e46d6b604bec3ed12adc10a9d16213d02bf
ajn404 <nhy1423238345@163.com>
2023-09-21 12:43:51 +0800

    feat: try try

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -10,66 +10,62 @@ import mdx from "@astrojs/mdx";
 import { remarkReadingTime } from "./plugin/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    port: 3333,
-  },
-  base: "/mono_notes",
-  build: {
-    assets: "static",
-    inlineStylesheets: "always",
-  },
+    server: {
+        port: 3333,
+    },
+    base: "/mono_notes",
+    build: {
+        assets: "static",
+        inlineStylesheets: "always",
+    },
 
-  site: "https://ajn404.github.io/mono_notes/", // replace this with your deployed domain
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    react({}),
-    sitemap(),
-    mdx(),
-  ],
-  markdown: {
-    remarkPlugins: [
-      [
-        remarkToc,
-        {
-          heading: "目录",
-        },
-      ],
-      [
-        remarkCollapse,
-        {
-          test: "脚本",
-          summary: str => "展开 " + str,
-        },
-      ],
-      remarkMath,
-      remarkReadingTime,
+    site: "https://ajn404.github.io/mono_notes/", // replace this with your deployed domain
+    integrations: [
+        tailwind({
+            config: {
+                applyBaseStyles: false,
+            },
+        }),
+        react({}),
+        sitemap(),
+        mdx(),
     ],
-    rehypePlugins: [rehypeKatex],
-    shikiConfig: {
-      theme: "one-dark-pro",
-      wrap: true,
+    markdown: {
+        remarkPlugins: [
+            [
+                remarkToc,
+                {
+                    heading: "目录",
+                },
+            ],
+            [
+                remarkCollapse,
+                {
+                    test: "脚本",
+                    summary: str => "展开 " + str,
+                },
+            ],
+            remarkMath,
+            remarkReadingTime,
+        ],
+        rehypePlugins: [rehypeKatex],
+        shikiConfig: {
+            theme: "one-dark-pro",
+            wrap: true,
+        },
+        extendDefaultPlugins: true,
     },
-    extendDefaultPlugins: true,
-  },
-  vite: {
-    optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-    },
-    build:{
-      rollupOptions:{
-        output:{
-          chunkFileNames:({name})=>{
-            if(name.indexOf('_')!==-1){
-              return "static/rename.[hash].js"
+    vite: {
+        optimizeDeps: {
+            exclude: ["@resvg/resvg-js"],
+        },
+        build: {
+            rollupOptions: {
+                output: {
+                    chunkFileNames: 'static/chunk.[hash].mjs',
+                    assetFileNames: 'assets/asset.[hash][extname]',
+                }
             }
-            return "static/[name].[hash].js"
-          }
         }
-      }
-    }
-  },
+    },
 });
