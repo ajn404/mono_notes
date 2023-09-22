c84d7bea7f668b632a86ed144d5b089a2832d1f9
ajn404 <nhy1423238345@163.com>
2023-09-22 10:10:44 +0800

    feat: tsconfig

--- docs/src/config.ts
+++ docs/src/config.ts
@@ -1,30 +1,30 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://ajn404.github.io/mono_notes/",
-  author: "ajn404",
-  desc: "纠结哥的笔记",
-  title: "笔记",
-  ogImage: "astropaper-og.jpg",
-  lightAndDarkMode: true,
-  postPerPage: 20,
+    website: "https://ajn404.github.io/mono_notes/",
+    author: "ajn404",
+    desc: "mono notes copying everything",
+    title: "笔记",
+    ogImage: "astropaper-og.jpg",
+    lightAndDarkMode: true,
+    postPerPage: 20,
 };
 
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
 
 export const LOGO_IMAGE = {
-  enable: false,
-  svg: true,
-  width: 216,
-  height: 46,
+    enable: false,
+    svg: true,
+    width: 216,
+    height: 46,
 };
 
 export const SOCIALS: SocialObjects = [
-  {
-    name: "Github",
-    href: "https://github.com/ajn404/mono_notes",
-    linkTitle: ` ${SITE.title}的代码仓库`,
-    active: true,
-  },
- 
+    {
+        name: "Github",
+        href: "https://github.com/ajn404/mono_notes",
+        linkTitle: ` ${SITE.title}的代码仓库`,
+        active: true,
+    },
+
 ];
