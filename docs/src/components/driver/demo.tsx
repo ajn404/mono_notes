9653f379804078091ad2d146a60942b7c9ddb67e
ajn404 <nhy1423238345@163.com>
2023-10-08 18:30:20 +0800

    feat: driverdriverjs

---
+++ docs/src/components/driver/demo.tsx
@@ -0,0 +1,44 @@
+import { useEffect } from "react"
+import { driver } from "driver.js";
+import "driver.js/dist/driver.css";
+
+interface Element {
+    title: string,
+    description: string,
+    element: string
+}
+
+interface Pros {
+    // elements?: Array<Element>
+    elements?: string
+}
+export default ({ elements }: Pros) => {
+    useEffect(() => {
+        if (elements && elements?.length > 0) {
+            // JSON.parse(elements).map((item: Element) => {
+            //     const driverObj = driver({
+            //         showProgress: true,
+            //         steps: JSON.parse(elements)
+            //     });
+            //     driverObj.highlight({
+            //         element: item.element,
+            //         popover: {
+            //             title: item.title,
+            //             description: item.description
+            //         }
+            //     });
+            // })
+
+            const driverObj = driver({
+                showProgress: true,
+                steps: JSON.parse(elements)
+            });
+            driverObj.drive();
+        }
+        return () => {
+        }
+    })
+
+    return
+    <></>
+}
